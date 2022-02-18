2e1d2443b5f5d738c56fe120cf28369e8c3aef0c
FFx0001 <vip.ffx0001@mail.ru>
2022-02-18 07:05:11 +0300

    #fix laravel

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         /*
          * PDF Provider
          */
-     //   Barryvdh\DomPDF\ServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -234,7 +234,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-       // 'PDF' => Barryvdh\DomPDF\Facade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
